7e8c1251e92a852722564a0dddd8fa060a35319e
ejcal9 <ejcalsado@gmail.com>
2023-05-23 01:30:44 +0800

    Update app.py

--- app.py
+++ app.py
@@ -26,9 +26,9 @@ def upload_predict(upload_image, model):
         image = ImageOps.fit(upload_image, size, Image.ANTIALIAS)
         image = np.asarray(image)
         img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        img_resize = cv2.resize(img, dsize=(7, 7),512)
+       
         
-        img_reshape = img_resize[np.newaxis,...]
+        img_reshape = img[np.newaxis,...]
     
         prediction = model.predict(img_reshape)
         pred_class=decode_predictions(prediction,top=1)
