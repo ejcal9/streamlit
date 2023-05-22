a5db20a60494297c538e0a1f37e8dc0a46a635d4
ejcal9 <ejcalsado@gmail.com>
2023-05-23 01:33:57 +0800

    Update app.py

--- app.py
+++ app.py
@@ -19,27 +19,35 @@ st.write("""
  
 file = st.file_uploader("Upload the image to be classified", type=["jpg", "png"])
 st.set_option('deprecation.showfileUploaderEncoding', False)
+
+def import_and_predict(image_data,model):
+    size=(64,64)
+    image=ImageOps.fit(image_data,size,Image.ANTIALIAS)
+    img=np.asarray(image)
+    img_reshape=img[np.newaxis,...]
+    prediction=model.predict(img_reshape)
+    return prediction
  
-def upload_predict(upload_image, model):
+#def upload_predict(upload_image, model):
     
-        size = (64,64)    
-        image = ImageOps.fit(upload_image, size, Image.ANTIALIAS)
-        image = np.asarray(image)
-        img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+   #     size = (64,64)    
+   #     image = ImageOps.fit(upload_image, size, Image.ANTIALIAS)
+   #     image = np.asarray(image)
+   #     img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
        
         
-        img_reshape = img[np.newaxis,...]
+   #     img_reshape = img[np.newaxis,...]
     
-        prediction = model.predict(img_reshape)
-        pred_class=decode_predictions(prediction,top=1)
+    #    prediction = model.predict(img_reshape)
+    #    pred_class=decode_predictions(prediction,top=1)
         
-        return pred_class
+    #    return pred_class
 if file is None:
     st.text("Please upload an image file")
 else:
     image = Image.open(file)
     st.image(image, use_column_width=True)
-    predictions = upload_predict(image, model)
+    predictions = import_and_predict(image, model)
     image_class = ['cat','dog','rat','house', 'fan', 'phone', 'horse', 'bear']
     st.write("The image is classified as",image_class)
     print("The image is classified as ",image_class)
